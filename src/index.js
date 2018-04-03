5ec3ef14b529229009502903b0da4774f1c654a9
silvestrevivo <silvestrevivomillan@gmail.com>
2018-04-03 13:23:12 +0200

    catching errors properly with axios

--- src/index.js
+++ src/index.js
@@ -9,7 +9,20 @@ btnGetTodo1.addEventListener('click', () => {
 
   axios.get('https://jsonplaceholder.typicode.com/todos')
     .then(response => resultElement.innerHTML = generateSuccessHTMLOutput(response))
-    .catch(error => resultElement.innerHTML = generateErrorHTMLOutput(error))
+    .catch(error => {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // we get information about the request: an error on different ways
+        resultElement.innerHTML = generateErrorHTMLOutput(error)
+      } else if (error.request) {
+        // The request was made but no response was received
+        // This happens when we do not have a network connection or maybe
+        // the UrlBase was not good typed. Error response does not catch it
+        resultElement.innerHTML = generateErrorNetworkHTMLOutput(error)
+      } else {
+        console.log('Error', error.message);
+      }
+    })
 });
 
 // GetPost with parameters
@@ -25,7 +38,20 @@ btnGetTodo2.addEventListener('click', () => {
     }
   })
     .then(response => resultElement.innerHTML = generateSuccessHTMLOutput(response))
-    .catch(error => resultElement.innerHTML = generateErrorHTMLOutput(error))
+    .catch(error => {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // we get information about the request: an error on different ways
+        resultElement.innerHTML = generateErrorHTMLOutput(error)
+      } else if (error.request) {
+        // The request was made but no response was received
+        // This happens when we do not have a network connection or maybe
+        // the UrlBase was not good typed. Error response does not catch it
+        resultElement.innerHTML = generateErrorNetworkHTMLOutput(error)
+      } else {
+        console.log('Error', error.message);
+      }
+    })
 });
 
 // PostPost
@@ -42,8 +68,23 @@ submitPost.addEventListener('click', (e) => {
     completed: false
   })
     .then(response => resultElement.innerHTML = generateSuccessHTMLOutput(response))
-    .catch(error => resultElement.innerHTML = generateErrorHTMLOutput(error))
+    .catch(error => {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // we get information about the request: an error on different ways
+        resultElement.innerHTML = generateErrorHTMLOutput(error)
+      } else if (error.request) {
+        // The request was made but no response was received
+        // This happens when we do not have a network connection or maybe
+        // the UrlBase was not good typed. Error response does not catch it
+        resultElement.innerHTML = generateErrorNetworkHTMLOutput(error)
+      } else {
+        console.log('Error', error.message);
+      }
+    })
 });
+
+////////////////////////////// Helper Functions /////////////////////////////////
 
 // Fuctions for OutPut
 const generateSuccessHTMLOutput = response =>
@@ -66,6 +107,11 @@ const generateErrorHTMLOutput = error =>
   <pre>${JSON.stringify(error.response.headers, null, '\t')}</pre>
   <h5>Data:</h5>
   <pre>${JSON.stringify(error.response.data, null, '\t')}</pre>`
+
+const generateErrorNetworkHTMLOutput = error =>
+  `<h4>Result</h4>
+  <h5>Message</h5>
+  <pre>NetWork error or wrong URL: ${!!error.request}</pre>`
 
 // Function to Clear Outputs
 var clearOutput = document.querySelectorAll('.btn-warning');
