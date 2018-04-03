f4c870e89f6cd2a850d1fab6e4e1e14494cde4f6
silvestrevivo <silvestrevivomillan@gmail.com>
2018-04-03 11:06:18 +0200

    application updated with ES6 syntax

--- src/index.js
+++ src/index.js
@@ -1,53 +1,22 @@
 import '../assets/sass/style.scss';
 import axios from 'axios';
 
+// GetPost
 const btnGetTodo1 = document.getElementById('buttonGetTodo1');
-btnGetTodo1.addEventListener('click', performGetRequest1)
-
-
-function performGetRequest1() {
-  console.log('yes')
-  var resultElement = document.getElementById('getResult1');
+btnGetTodo1.addEventListener('click', () => {
+  const resultElement = document.getElementById('getResult1');
   resultElement.innerHTML = '';
 
   axios.get('https://jsonplaceholder.typicode.com/todos')
-    .then(function (response) {
-      resultElement.innerHTML = generateSuccessHTMLOutput(response);
-    })
-    .catch(function (error) {
-      resultElement.innerHTML = generateErrorHTMLOutput(error);
-    })
-}
+    .then(response => resultElement.innerHTML = generateSuccessHTMLOutput(response))
+    .catch(error => resultElement.innerHTML = generateErrorHTMLOutput(error))
+});
 
-function generateSuccessHTMLOutput(response) {
-  return `<h4>Result</h4>
-          <h5>Status:</h5>
-          <pre>${response.status} ${response.statusText}</pre>
-          <h5>Headers:</h5>
-          <pre>${JSON.stringify(response.headers, null, '\t')}</pre>
-          <h5>Data:</h5>
-          <pre>${JSON.stringify(response.data, null, '\t')}</pre>`;
-}
-
-function generateErrorHTMLOutput(error) {
-  return `<h4>Result</h4>
-          <h5>Message</h5>
-          <pre>${error.message}</pre>
-          <h5>Status:</h5>
-          <pre>${error.response.status} ${error.response.statusText}</pre>
-          <h5>Headers:</h5>
-          <pre>${JSON.stringify(error.response.headers, null, '\t')}</pre>
-          <h5>Data:</h5>
-          <pre>${JSON.stringify(error.response.data, null, '\t')}</pre>`;
-}
-
-
+// GetPost with parameters
 const btnGetTodo2 = document.getElementById('buttonGetTodo2');
-btnGetTodo2.addEventListener('click', performGetRequest2)
-
-function performGetRequest2() {
-  var todoId = document.getElementById('todoId').value;
-  var resultElement = document.getElementById('getResult2');
+btnGetTodo2.addEventListener('click', () => {
+  let todoId = document.getElementById('todoId').value;
+  const resultElement = document.getElementById('getResult2');
   resultElement.innerHTML = '';
 
   axios.get('https://jsonplaceholder.typicode.com/todos', {
@@ -55,21 +24,16 @@ function performGetRequest2() {
       id: todoId
     }
   })
-    .then(function (response) {
-      resultElement.innerHTML = generateSuccessHTMLOutput(response);
-    })
-    .catch(function (error) {
-      resultElement.innerHTML = generateErrorHTMLOutput(error);
-    })
-}
+    .then(response => resultElement.innerHTML = generateSuccessHTMLOutput(response))
+    .catch(error => resultElement.innerHTML = generateErrorHTMLOutput(error))
+});
 
-
+// PostPost
 const submitPost = document.getElementById('submitPost');
-submitPost.addEventListener('click', performPostRequest);
-
-function performPostRequest(e) {
-  var resultElement = document.getElementById('postResult');
-  var todoTitle = document.getElementById('todoTitle').value;
+submitPost.addEventListener('click', (e) => {
+  e.preventDefault();
+  let todoTitle = document.getElementById('todoTitle').value;
+  const resultElement = document.getElementById('postResult');
   resultElement.innerHTML = '';
 
   axios.post('https://jsonplaceholder.typicode.com/todos', {
@@ -77,20 +41,36 @@ function performPostRequest(e) {
     title: todoTitle,
     completed: false
   })
-    .then(function (response) {
-      resultElement.innerHTML = generateSuccessHTMLOutput(response);
-    })
-    .catch(function (error) {
-      resultElement.innerHTML = generateErrorHTMLOutput(error);
-    })
-  e.preventDefault();
-}
+    .then(response => resultElement.innerHTML = generateSuccessHTMLOutput(response))
+    .catch(error => resultElement.innerHTML = generateErrorHTMLOutput(error))
+});
+
+// Fuctions for OutPut
+const generateSuccessHTMLOutput = response =>
+  `<h4>Result</h4>
+  <h5>Status:</h5>
+  <pre>${response.status} ${response.statusText}</pre>
+  <h5>Headers:</h5>
+  <pre>${JSON.stringify(response.headers, null, '\t')}</pre>
+  <h5>Data:</h5>
+  <pre>${JSON.stringify(response.data, null, '\t')}</pre>`
 
 
+const generateErrorHTMLOutput = error =>
+  `<h4>Result</h4>
+  <h5>Message</h5>
+  <pre>${error.message}</pre>
+  <h5>Status:</h5>
+  <pre>${error.response.status} ${error.response.statusText}</pre>
+  <h5>Headers:</h5>
+  <pre>${JSON.stringify(error.response.headers, null, '\t')}</pre>
+  <h5>Data:</h5>
+  <pre>${JSON.stringify(error.response.data, null, '\t')}</pre>`
+
+// Function to Clear Outputs
 var clearOutput = document.querySelectorAll('.btn-warning');
-clearOutput.forEach(item => item.addEventListener('click', function () {
+clearOutput.forEach(item => item.addEventListener('click', () => {
   document.getElementById('getResult1').innerHTML = ' ';
   document.getElementById('getResult2').innerHTML = ' ';
-  document.getElementById('postRsult').innerHTML = ' ';
+  document.getElementById('postResult').innerHTML = ' ';
 }))
-
